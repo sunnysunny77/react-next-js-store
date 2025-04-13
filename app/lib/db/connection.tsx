06ada36b05d78b72434892643d7ad396e3ef4112
daniel <contractor@lateralaspect.com.au>
2025-04-14 02:56:06 +0800

    Update connection.tsx

--- app/lib/db/connection.tsx
+++ app/lib/db/connection.tsx
@@ -1,6 +1,7 @@
 import mysql from 'mysql2/promise';
 
-const Connection = mysql.createPool({
+// on my server i need the pool
+const pool = mysql.createPool({
   host: process.env.REACT_APP_MYSQL_HOST,
   user: process.env.REACT_APP_MYSQL_USER,
   database: process.env.REACT_APP_MYSQL_DATABASE,
@@ -12,6 +13,13 @@ const Connection = mysql.createPool({
   queueLimit: 0,
   enableKeepAlive: true,
   keepAliveInitialDelay: 0,
+});
+
+const Connection = await mysql.createConnection({
+  host: process.env.REACT_APP_MYSQL_HOST,
+  user: process.env.REACT_APP_MYSQL_USER,
+  database: process.env.REACT_APP_MYSQL_DATABASE,
+  password: process.env.REACT_APP_MYSQL_PASSWORD,
 });
 
 export default Connection;
