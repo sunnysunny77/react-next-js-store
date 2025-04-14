e975469a1a73725e4e4646bc48e81d6856608247
daniel <contractor@lateralaspect.com.au>
2025-04-14 16:33:47 +0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,57 +2,59 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(request: NextRequest) {
 
-    const token = request.cookies.get("Store-App");
+  const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
 
-    if (request.nextUrl.pathname === "/store" &&  !token) {
+  const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' ${process.env.NODE_ENV === "development" ? "'unsafe-eval'" : ""};
+    style-src 'self' 'unsafe-inline' https://*.paypal.com;
+    img-src 'self' blob: data: https://*.paypalobjects.com;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    connect-src 'self' https://www.sandbox.paypal.com;
+    form-action 'self';
+    frame-src https://www.sandbox.paypal.com;
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+  `;
 
-      return NextResponse.redirect(new URL("/", request.url));
+  const contentSecurityPolicyHeaderValue = cspHeader.replace(/\s{2,}/g, " ").trim();
 
-    };
+  const token = request.cookies.get("Store-App");
 
-    if (request.nextUrl.pathname === "/auth" &&  token) {
+  if (request.nextUrl.pathname === "/store" &&  !token) {
 
-      return NextResponse.redirect(new URL("/store", request.url))
-
-    };
-
-    const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
-
-    const cspHeader = `
-      default-src 'self';
-      script-src 'self' 'nonce-${nonce}' 'strict-dynamic' ${process.env.NODE_ENV === "development" ? "'unsafe-eval'" : ""};
-      style-src 'self' 'unsafe-inline' https://*.paypal.com;
-      img-src 'self' blob: data: https://*.paypalobjects.com;
-      font-src 'self';
-      object-src 'none';
-      base-uri 'self';
-      connect-src 'self' https://www.sandbox.paypal.com;
-      form-action 'self';
-      frame-src https://www.sandbox.paypal.com;
-      frame-ancestors 'none';
-      upgrade-insecure-requests;
-    `;
-
-    const contentSecurityPolicyHeaderValue = cspHeader.replace(/\s{2,}/g, " ").trim();
-
-    const requestHeaders = new Headers(request.headers);
-    requestHeaders.set("x-nonce", nonce);
-    requestHeaders.set(
-      'Content-Security-Policy', contentSecurityPolicyHeaderValue
-    );
-
-    const response = NextResponse.next({
-      request: {
-        headers: requestHeaders,
-      },
-    });
-
-    response.headers.set(
-      'Content-Security-Policy',contentSecurityPolicyHeaderValue
-    );
-
+    const response = NextResponse.redirect(new URL("/", request.url));
+    response.headers.set('Content-Security-Policy', contentSecurityPolicyHeaderValue);
     return response;
   };
+
+  if (request.nextUrl.pathname === "/auth" &&  token) {
+
+    const response = NextResponse.redirect(new URL("/store", request.url));
+    response.headers.set('Content-Security-Policy', contentSecurityPolicyHeaderValue);
+    return response;
+  };
+
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-nonce", nonce);
+  requestHeaders.set(
+    'Content-Security-Policy', contentSecurityPolicyHeaderValue
+  );
+
+  const response = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
+
+  response.headers.set(
+    'Content-Security-Policy',contentSecurityPolicyHeaderValue
+  );
+
+  return response;
+};
 
 export const config = {
   matcher: [
