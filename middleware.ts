a281d466a9bf238dbd9a9a489c56e931c0ff3da5
daniel <contractor@lateralaspect.com.au>
2025-04-20 12:38:56 +0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,18 @@
-import {NextRequest, NextResponse} from 'next/server'
+import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
+
+  const token = request.cookies.get("Store-App");
+
+  if (request.nextUrl.pathname === "/store" &&  !token) {
+
+    return NextResponse.redirect(new URL("/", request.url));
+  };
+
+  if (request.nextUrl.pathname === "/auth" &&  token) {
+
+    return NextResponse.redirect(new URL("/store", request.url));
+  };
 
   const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
 
@@ -21,48 +33,38 @@ export function middleware(request: NextRequest) {
 
   const contentSecurityPolicyHeaderValue = cspHeader.replace(/\s{2,}/g, " ").trim();
 
-  const token = request.cookies.get("Store-App");
-
-  if (request.nextUrl.pathname === "/store" &&  !token) {
-
-    const response = NextResponse.redirect(new URL("/", request.url));
-    response.headers.set("Content-Security-Policy", contentSecurityPolicyHeaderValue);
-    return response;
-  };
-
-  if (request.nextUrl.pathname === "/auth" &&  token) {
-
-    const response = NextResponse.redirect(new URL("/store", request.url));
-    response.headers.set("Content-Security-Policy", contentSecurityPolicyHeaderValue);
-    return response;
-  };
-
   const requestHeaders = new Headers(request.headers);
+
   requestHeaders.set("x-nonce", nonce);
+
   requestHeaders.set(
-    "Content-Security-Policy", contentSecurityPolicyHeaderValue
+
+    'Content-Security-Policy', contentSecurityPolicyHeaderValue
   );
 
   const response = NextResponse.next({
+
     request: {
       headers: requestHeaders,
     },
   });
 
   response.headers.set(
-    "Content-Security-Policy",contentSecurityPolicyHeaderValue
+
+    'Content-Security-Policy',contentSecurityPolicyHeaderValue
   );
 
   return response;
 };
 
 export const config = {
+
   matcher: [
     {
       source: "/((?!api|_next/static|_next/image|favicon.ico).*)",
       missing: [
-        {type: "header", key: "next-router-prefetch"},
-        {type: "header", key: "purpose", value: "prefetch"},
+        { type: "header", key: "next-router-prefetch" },
+        { type: "header", key: "purpose", value: "prefetch" },
       ],
     },
   ],
